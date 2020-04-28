221845d122f981ec0cef34531a2ad9e361aea84c
inman99 <inman15@163.com>
2020-04-28 23:25:57 +0900

    fix1

---
+++ glfwcpp/KKHeader.h
@@ -0,0 +1,14 @@
+//
+//  KKHeader.h
+//  glfwcpp
+//
+//  Created by inman on 2020/04/28.
+//  Copyright © 2020 inman. All rights reserved.
+//
+
+#ifndef KKHeader_h
+#define KKHeader_h
+
+#include <iostream>
+
+#endif /* KKHeader_h */
--- glfwcpp/Triangle/main.cpp
+++ glfwcpp/Triangle/main.cpp
@@ -4,6 +4,8 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
+#include "KKHeader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
